ee417c3da845956ebe6629ba5e832a798c5a4bff
kennethleanda <leandakenneth@gmail.com>
2022-06-01 14:25:35 +0800

    changes pt3

--- database/migrations/2022_05_27_021439_create_cart_table.php
+++ database/migrations/2022_05_27_021439_create_cart_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('product_id');
             $table->integer('user_id');
+            $table->integer('status')->default(1);
             $table->timestamps();
         });
     }
